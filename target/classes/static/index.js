4a2dfdefe3bec6e5ebcdfcd44514391df8e702a7
Harsha-326 <harshavardhanvelakanti@gmail.com>
2026-07-18 12:07:41 +0530

    toggle user

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,1 +1,13 @@
 alert("FAHHHHHHHHHHH");
+console.log("Why are we here");
+
+var users={
+{
+    "name": "Harsha",
+    "Gender": "Male",
+    "img": "john.png"
+},
+{   "name": "Harsha",
+    "Gender": "Male",
+    "img": "john.png"
+}}
